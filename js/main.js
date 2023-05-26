2511b32179714204bd59deb44f595300cb916fa1
Пользователь <milausik@ukr.net>
2023-05-26 16:32:57 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -50,3 +50,18 @@
       moreText1.style.display = "inline";
     }
   }
+
+  $(document).ready(function () {
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToScroll: 1,
+      arrows: true,
+      variableWidth: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+
+
+
+
