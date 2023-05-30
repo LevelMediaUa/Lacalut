c98ba9460dd50f024b5c63292e76a8e1764d89ae
Пользователь <milausik@ukr.net>
2023-05-30 14:22:57 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -62,3 +62,12 @@
     });
   });
 
+
+
+  const mask = document.querySelector('.mask');
+  window.addEventListener('load', () => {
+    mask.classList.add('hide');
+    setTimeout(() => { 
+        mask.remove();
+    }, 600);
+});
